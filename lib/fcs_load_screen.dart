40d832b0c3eac6f82624d12ed6d813d7e5ae7198
Thiago Monteiro <tsmonteiro@gmail.com>
2024-07-11 16:09:32 -0300

    dbg

--- lib/fcs_load_screen.dart
+++ lib/fcs_load_screen.dart
@@ -286,15 +286,16 @@ class _FcsLoadScreenState extends State<FcsLoadScreen>{
     var compTask = await factory.taskService.get(taskId) as sci.RunComputationTask;
     sci.CompositeRelation rel = compTask.computedRelation as sci.CompositeRelation;
     // print(rel.toJson());
-    sci.Schema sch = await factory.tableSchemaService.get(rel.joinOperators[1].rightRelation.id);
-    // print(sch.toJson());
-    print("Selecting");
-    sci.Table tbl = await factory.tableSchemaService.select(sch.id, ["filename", ".content"], 0, 1);
-    print(tbl.toJson());
+    sci.CompositeRelation cr = rel.joinOperators[0].rightRelation as sci.CompositeRelation;
+    sci.Schema sch = await factory.tableSchemaService.get(cr.joinOperators[0].rightRelation.id);
+    print(sch.toJson());
+    // print("Selecting");
+    // sci.Table tbl = await factory.tableSchemaService.select(sch.id, ["filename", ".content"], 0, 1);
+    // print(tbl.toJson());
     // _tryToPrint(rel.mainRelation.id, "rel.mainRelation.id");
-    // sci.CompositeRelation cr = rel.joinOperators[0].rightRelation as sci.CompositeRelation;
+    // 
     // _tryToPrint(cr.joinOperators[0].rightRelation.id, "rel.joinOperators[0]...rightRelation.id"); 
-    // _tryToPrint(rel.joinOperators[1].rightRelation.id, "rel.joinOperators[1].rightRelation.id"); // Channel annotation
+    // _tryToPrint(rel.joinOperators[1].rightRelation.id, "rel.joinOperators[1].rightRelation.id"); // Summary
     
   }
 
