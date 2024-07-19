e887e94e5627cc2e4fd711b3e744e8b98378ac68
Thiago Monteiro <tsmonteiro@gmail.com>
2024-07-19 10:58:35 -0300

    dbg

--- lib/fcs_load_screen.dart
+++ lib/fcs_load_screen.dart
@@ -307,7 +307,9 @@ class _FcsLoadScreenState extends State<FcsLoadScreen>{
 
 
   void _getComputedRelation(String taskId) async{
+    // Works for zip file...
     var compTask = await factory.taskService.get(taskId) as sci.RunComputationTask;
+    print(compTask.computedRelation.toJson());
     sci.CompositeRelation rel = compTask.computedRelation as sci.CompositeRelation;
     sci.CompositeRelation cr = rel.joinOperators[0].rightRelation as sci.CompositeRelation;
     sci.Schema measurementSch = await factory.tableSchemaService.get(cr.mainRelation.id);
