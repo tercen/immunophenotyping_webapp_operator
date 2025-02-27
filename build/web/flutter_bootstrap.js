157211cc00df0762bff3bd2f37537bec22c5329e
Thiago <tsmonteiro@gmail.com>
2025-02-27 09:21:18 -0300

    no delete

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3876359472"
+    serviceWorkerVersion: "2451033365"
   }
 });
